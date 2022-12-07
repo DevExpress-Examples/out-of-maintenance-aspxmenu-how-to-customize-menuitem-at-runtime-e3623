f905d8e3bed212bcd39de33c4080fe2458184779
DevExpressExampleBot <fakeEmail>
2022-12-07 15:50:50 +0400

    Source auto update [skip ci]

--- CS/WebSite/Default.aspx.cs
+++ CS/WebSite/Default.aspx.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using DevExpress.Web.ASPxMenu;
-using DevExpress.Web.ASPxSiteMapControl;
+using DevExpress.Web;
 
 
 public partial class _Default : System.Web.UI.Page {
     protected void Page_Load (object sender, EventArgs e) {
 
     }
-    protected void ASPxMenu2_ItemDataBound (object source, DevExpress.Web.ASPxMenu.MenuItemEventArgs e) {
+    protected void ASPxMenu2_ItemDataBound (object source, DevExpress.Web.MenuItemEventArgs e) {
         SiteMapNode node = e.Item.DataItem as SiteMapNode;
         if (node != null) {
             e.Item.Text = "<div><b>" + e.Item.Text + "</b> <br />" + node["description"] + "</div>"; // or some another tags like <a href=''>
